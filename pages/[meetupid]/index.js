4bab7c89b4bb735f457c931944e68ada0183b755
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-11-28 12:25:25 -0700

    added custom components and css styling

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,17 @@
+
+import MeetUpDetail from "../../components/meetups/meetupDetail"
+
+
+function MeetUpDetails(){
+    return (
+    <MeetUpDetail
+   
+        image= "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+        title="First Meetup"
+        address="Some Street 5, Some city"
+        description="this is a first meetup"
+ />
+    )
+}
+
+export default MeetUpDetails
