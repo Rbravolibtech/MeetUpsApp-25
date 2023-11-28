f961990fe0bc94f7bf4e67efe5ae4a4c1edfcd90
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-11-28 14:09:46 -0700

    working with mongo db

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,3 +1,5 @@
+import {MongoClient} from "mongodb";
+
 // /api/new-meetup
 //POST /api/new-meetup
 
@@ -6,7 +8,21 @@ function handler(req, res) {
     const data = req.body;
 
 
-    const { title, image, address, description} = data
+    
+
+   const client =  MongoClient.connect("mongodb+srv://<username>:<password>@cluster0.rfre0.mongodb.net/?")
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+  const result = meetupsCollection.insertOne(data);
+
+  console.log(result);
+
+  client.close();
+
+  res.status(201.).json({message: "meetup inserted"})
+  
     }
 }
 
